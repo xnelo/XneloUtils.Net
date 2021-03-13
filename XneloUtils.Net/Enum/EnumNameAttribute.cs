86be8be750b2559b141363bb792ed295412c63cd
Spencer Hoffa <xnelo.productions@gmail.com>
2021-03-13 00:10:24 -0700

    Added enumeration attributes.

---
+++ XneloUtils.Net/Enum/EnumNameAttribute.cs
@@ -0,0 +1,23 @@
+﻿#region Copyright (c) 2021 Spencer Hoffa
+// \file EnumNameAttribute.cs
+// \author Spencer Hoffa
+// \copyright \link LICENSE.md MIT License\endlink 2021 Spencer Hoffa 
+#endregion
+
+using System;
+
+namespace XneloUtils.Net.Enum
+{
+	[AttributeUsage(AttributeTargets.Field)]
+	public class EnumNameAttribute: Attribute
+	{
+		public EnumNameAttribute(string name, string description = null)
+		{
+			Name = name;
+			Description = description;
+		}
+
+		public string Name { get; private set; }
+		public string Description { get; private set; }
+	}
+}
