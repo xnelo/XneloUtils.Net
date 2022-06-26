a99b851ec384a1fe907e105b004e089973fb0bf9
Spencer Hoffa <s_hoffa@hotmail.com>
2022-06-26 17:31:08 -0600

    Added bootstrapper to XneloUtils

---
+++ XneloUtils.Desktop.Bootstrap.Autofac.Net/IAutofacRegistry.cs
@@ -0,0 +1,15 @@
+﻿#region Copyright (c) 2022 Spencer Hoffa
+// \file IAutofacRegistry.cs
+// \author Spencer Hoffa
+// \copyright \link LICENSE.md MIT License\endlink 2022 Spencer Hoffa 
+#endregion
+
+using Autofac;
+
+namespace XneloUtils.Desktop.Bootstrap.Autofac.Net
+{
+	public interface IAutofacRegistry
+	{
+		void Register(ContainerBuilder builder);
+	}
+}
